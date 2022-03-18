fc7c260c2b849118ced7c0795631a339e276d629
EUNJEONGMUN <munej26@gmail.com>
2022-03-18 14:12:00 +0900

    휴대폰 번호 정규식 수정, dependencies 수정

--- api-server-spring-boot-final 2/src/main/java/com/example/demo/src/user/model/Req/PostMessageReq.java
+++ api-server-spring-boot-final 2/src/main/java/com/example/demo/src/user/model/Req/PostMessageReq.java
@@ -16,6 +16,7 @@ import javax.validation.constraints.Pattern;
 public class PostMessageReq {
 
     @NotBlank(message = "휴대폰 번호를 입력해주세요")
-    @Pattern(regexp = "/^010([0-9]{3,4})([0-9]{4})$/", message = "휴대폰 번호 형식을 확인해주세요.")
+//    @Pattern(regexp = "/^010([0-9]{3,4})([0-9]{4})$/", message = "휴대폰 번호 형식을 확인해주세요.")
+    @Pattern(regexp = "^010(\\d{3}|\\d{4})(\\d{4})$", message = "휴대폰 번호 형식을 확인해주세요.")
     private String phoneNumber;
 }
